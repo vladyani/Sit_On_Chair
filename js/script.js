7a8b898c50441ae40b58f6d16582ff9df1a779a4
Vlad <vladyslavyantsov@gmail.com>
2018-04-07 14:25:34 +0200

    drop menu done

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var mainMenu = document.querySelector('.main_menu');
+    var dropMenu = document.querySelector('.drop_menu');
+
+    function showDropMenu() {
+        if (dropMenu.style.display === 'none') {
+            dropMenu.style.animation = 'showMenu 1s linear 1';
+            dropMenu.style.display = 'block';
+        } else {
+            //dropMenu.style.animation = 'hideMenu 1s linear 1';
+            dropMenu.style.display = 'none';
+        }
+    }
+
+    mainMenu.addEventListener('click', showDropMenu);
+});
